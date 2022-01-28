cf8efa7d0ff46da7506c4630cd3ba7f21a0a62be
James Morgan <james.morgan@sydney.edu.au>
2022-01-28 14:40:45 +1100

    rewriting extended persistence

---
+++ R/extendedPersistence.R
@@ -0,0 +1,10 @@
+extendedPersistence <- function(bdryCurves,
+                                imgName,
+                                directions,
+                                tolerance = 1/sqrt(2),
+                                saveOutput = FALSE,
+                                outputDir = NULL,
+                                fName = NULL,
+                                verbose = TRUE) {
+    
+}
