f1e52fe2903d7af38073c97db93fea9d5c1e14ac
James Morgan <james.morgan@sydney.edu.au>
2022-01-31 18:17:40 +1100

    fixed plotting bug

--- R/plot.extDiagram.R
+++ R/plot.extDiagram.R
@@ -1,113 +1,61 @@
 #' @export
-plot.extDiagram <- function(x, maxDirection,
-                            directions = NULL,
+plot.extDiagram <- function(x,
                             barcode = FALSE,
                             rotated = FALSE,
                             col = NULL,
+                            showLegend = FALSE,
                             legendPos = NULL,
                             lab.line = 2.2, ...) {
     # Add checks for diagram list
     # Add checks for diagram Names
-    
-    if (is.null(directions)) {
-        directions <- 1:maxDirection
-    } else if (length(directions) == 1) {
-        directions <- c(directions)
+    X = vector()
+    for (i in 2:5) {
+        if (length(x[[i]]) > 2) {
+            sortedx <- x[[i]][order(x[[i]][,1], decreasing = FALSE), ]
+            sortedx <- cbind(rep(i-1, length(sortedx)/2), sortedx)
+            X <- rbind(X, sortedx)
+        } else if (length(x[[i]]) == 2) {
+            sortedx <- cbind(i-1, x[[i]])
+            X <- rbind(X, sortedx)
+        }
     }
- 
-    for (d in directions) {
-        X = vector()
+    
+    if (is.null(col)) {
+        col <- c(rep("#000000", length(which(X[,1] == 1))),
+                 rep("#e69f00", length(which(X[,1] == 2))),
+                 rep("#009e73", length(which(X[,1] == 3))),
+                 rep("#d55e00", length(which(X[,1] == 4))))
+    }
+    
+    if (barcode) {
+        birth <- X[,2]
+        death <- X[,3]
         
-        imgName <- head(strsplit(x[[1]][[d]], "-")[[1]], -1)
-        pName <- imgName[1]
-        if (length(imgName) > 1) {
-            for (i in 2:length(imgName)) {
-                pName <- paste(pName, imgName[i], sep = " ")
-            }
-        }
-        #print(pName)
-        denom <- as.character(maxDirection/2)
-        numer <- as.character(d)
-        directionName <- bquote(.(pName) ~ (.(numer) * pi / .(denom)))
+        first <- min(birth)
+        last <- max(death)
         
-        for (i in 2:5) {
-
-            if (length(x[[i]][[d]]) > 2) {
-                if (i %% 2 == 1) {
-                    sortedx <- x[[i]][[d]][order(x[[i]][[d]][,1], decreasing = TRUE),]
-                } else {
-                    sortedx <- x[[i]][[d]][order(x[[i]][[d]][,1], decreasing = FALSE),]
-                }
-                sortedx <- cbind(rep(i-1, length(sortedx)/2),
-                                 sortedx)
-                X <- rbind(X, sortedx)
-            } else if (length(x[[i]][[d]]) == 2) {
-                sortedx <- cbind(i-1, x[[i]][[d]])
-                X <- rbind(X, sortedx)
-            }
-        }
+        n <- length(birth)
         
-        if (length(X) > 0) {
-            plotLim <- c(min(X[,2:3]), max(X[,2:3]))
-        } else {
-            plotLim <- c(0,0)
-        }
+        graphics::plot(c(first,last),
+                       c(1,n+1),
+                       type = "n",
+                       xlab = "",
+                       ylab = "",
+                       xlim = c(first, last),
+                       ylim = c(0, n+1),
+                       xaxt = "n",
+                       yaxt = "n", ...)
         
-        if (is.null(col)) {
-            col <- vector()
-            for (i in 1:length(X[,1])) {
-                if (X[i,1] == 1) {
-                    col <- append(col, "#000000")
-                } else if (X[i,1] == 2) {
-                    col <- append(col, "#e69f00")
-                } else if (X[i,1] == 3) {
-                    col <- append(col, "#009e73")
-                } else {
-                    col <- append(col, "#d55e00")
-                }
-            }
-        }
+        graphics::axis(1)
         
-        if (barcode) {
-            birth <- X[,2]
-            death <- X[,3]
-            
-            first <- min(birth)
-            last <- max(death)
-            
-            n <- length(birth)
-            
-            if (is.null(legendPos)) {
-                legendPos <- "topright"
-            }
-            
-            graphics::plot(c(first, last),
-                           c(1,n+1),
-                           type = "n",
-                           xlab = "",
-                           ylab = "",
-                           xlim = c(first, last),
-                           ylim = c(0, n+1),
-                           xaxt = "n",
-                           yaxt = "n", ...)
-            
-            graphics::axis(1)
-            graphics::title(main = directionName,
-                            xlab = "height",
-                            line = lab.line)
-            graphics::segments(birth, 1:n,
-                               death, 1:n,
-                               lwd = rep(2,n),
-                               lty = rep(1,n),
-                               col = col)
-            graphics::legend(legendPos,
-                             legend = c(expression('Ord'[0]),
-                                        expression('Rel'[1]),
-                                        expression('Ext'[0]),
-                                        expression('Ext'[1])),
-                             col = col,
-                             lwd = 2,
-                             lty = 1)
-        }
+        graphics::title(xlab = "height",
+                        line = lab.line)
+        
+        graphics::segments(birth, 1:n,
+                           death, 1:n,
+                           lwd = rep(2,n),
+                           lty = rep(1,n),
+                           col = col)
+        
     }
 }
